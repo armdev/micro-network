9601ae199ad98b45f24bfaa5ce121fb50c3f3fad
armdev <armen.arzumanyan@gmail.com>
2017-09-03 19:44:01 +0400

    changing all

---
+++ full-node/src/main/java/io/project/application/FullNodeApplication.java
@@ -0,0 +1,32 @@
+package io.project.application;
+
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class FullNodeApplication implements GreetingController {
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullNodeApplication.class, args);
+    }
+
+    @Override
+    public String greeting() {
+        return String.format("Hello from '%s'!", eurekaClient.getApplication(appName).getName());
+    }
+}
---
+++ full-node/src/main/java/io/project/application/GreetingController.java
@@ -0,0 +1,10 @@
+package io.project.application;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public interface GreetingController {
+    
+    
+    @RequestMapping("/greeting")
+    String greeting();
+}
