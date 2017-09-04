77807556d56a0c4f12ef7caf3a3b6859610b3652
armdev <armen.arzumanyan@gmail.com>
2017-09-04 23:24:55 +0400

    changing all

--- core-node/src/main/java/io/project/core/application/GreetingController.java
+++ core-node/src/main/java/io/project/core/application/GreetingController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public interface GreetingController {
 
-    @RequestMapping("/healthcheck")
+    @RequestMapping("/api/logs/healthcheck")
     String greeting();
 }
