257e9147e2e98ec02abf6e7cd307fe39e3948b37
armdev <armen.arzumanyan@gmail.com>
2017-09-10 00:27:23 +0400

    changing all

--- core-node/src/main/java/io/project/core/application/GreetingClient.java
+++ core-node/src/main/java/io/project/core/application/GreetingClient.java
@@ -1,6 +1,6 @@
 package io.project.core.application;
 import org.springframework.cloud.netflix.feign.FeignClient;
 
-@FeignClient(name="full-node", url = "${full.node.url}")
+@FeignClient(name="fullnode", url = "${full.node.url}")
 public interface GreetingClient extends GreetingController {
 }
