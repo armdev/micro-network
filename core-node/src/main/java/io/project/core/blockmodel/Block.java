6f05acce3e3a970560ecd93b4553402a945d8618
armdev <armen.arzumanyan@gmail.com>
2017-09-10 23:38:49 +0400

    changing all

--- core-node/src/main/java/io/project/core/blockmodel/Block.java
+++ core-node/src/main/java/io/project/core/blockmodel/Block.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package io.project.core.blockmodel;
 
-import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.apache.commons.codec.digest.DigestUtils;
 
 /**
  *
@@ -32,30 +25,6 @@ public class Block {
 
     public Block(String data) {
         this.data = data;
-    }
-
-   
-
-    public Block generateNextBlock(String blockData) {
-        Block previousBlock = this.getLatestBlock();
-        String previousBlockIndex = previousBlock.getIndex();
-
-        String nextIndex = previousBlockIndex + 1;
-        Long nextTimestamp = new Date().getTime() / 1000;
-        String nextHash = this.calculateHash(nextIndex, previousBlock.getHash(), nextTimestamp.toString(), blockData);
-
-        return new Block(nextIndex, previousBlock.getHash(), nextTimestamp.toString(), blockData, nextHash);
-    }
-
-    public Block getLatestBlock() {
-        Block block = new Block(data);
-
-        return block;
-    }
-    
-     public String calculateHash(String index, String previousHash, String timestamp, String data) {
-        String sha256hex = DigestUtils.sha256Hex(index + previousHash + timestamp + data);
-        return sha256hex;
     }
 
 // https://medium.com/@lhartikk/a-blockchain-in-200-lines-of-code-963cc1cc0e54
