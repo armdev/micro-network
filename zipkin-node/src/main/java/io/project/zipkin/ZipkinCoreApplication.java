78c6d08a2e7805292866ef179b49e50095bd4ef7
ARMEN ARZUMANYAN <armen.arzumanyan.external@atos.net>
2017-10-10 18:43:13 +0400

    got to stable go

--- zipkin-node/src/main/java/io/project/zipkin/ZipkinCoreApplication.java
+++ zipkin-node/src/main/java/io/project/zipkin/ZipkinCoreApplication.java
@@ -3,12 +3,9 @@ package io.project.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
-
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
-//@EnableZipkinStreamServer
 @EnableZipkinServer
 @EnableEurekaClient
 public class ZipkinCoreApplication {
