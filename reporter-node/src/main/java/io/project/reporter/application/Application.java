a5c9e36719d297bc63df7a2d58670933beee0901
ARMEN ARZUMANYAN <armen.arzumanyan.external@atos.net>
2017-10-10 15:34:51 +0400

    up to date

--- reporter-node/src/main/java/io/project/reporter/application/Application.java
+++ reporter-node/src/main/java/io/project/reporter/application/Application.java
@@ -3,12 +3,30 @@ package io.project.reporter.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableAsync
+@EnableFeignClients
+@EnableMongoAuditing
+@EnableMongoRepositories
+@EnableScheduling
+@EnableHystrix
+@Import(SpringConfig.class)
+@ComponentScan(basePackages = {"io"}, excludeFilters = {
+@ComponentScan.Filter(Configuration.class)})
 public class Application {
 
     public static void main(String[] args) {
--- reporter-node/src/main/java/io/project/reporter/application/SleuthDemoApplication.java
+++ reporter-node/src/main/java/io/project/reporter/application/SleuthDemoApplication.java
@@ -33,32 +33,16 @@ public class SleuthDemoApplication {
     @Value("${server.port}")
     private int appPort;
 
-    @Value("${otherapp.port}")
-    private int otherappPort;
-
     @RequestMapping("/")
     public String home() {
         LOGGER.info("Request to / endpoint");
         return "Hello World";
     }
-
-    @RequestMapping("/getRemoteTime")
-    public String getRemoteTime() throws URISyntaxException {
-        LOGGER.info("Request to /getRemoteTime endpoint");
-
-        logService.log("Will call remote service");
-
-        ResponseEntity<String> forEntity = restTemplate.getForEntity(new URI("http://localhost:" + otherappPort + "/getTime"), String.class);
-        LOGGER.info("Got response code: {}", forEntity.getStatusCode().toString());
-        return "The remote time is: " + forEntity.getBody();
-    }
-
+  
     @RequestMapping("/getLocalTime")
     public String getLocalTime() throws URISyntaxException {
         LOGGER.info("Request to /getLocalTime endpoint");
-
         logService.log("Will call local service");
-
         ResponseEntity<String> forEntity = restTemplate.getForEntity(new URI("http://localhost:" + appPort + "/getTime"), String.class);
         LOGGER.info("Got response code: {}", forEntity.getStatusCode().toString());
         return "The localtime is: " + forEntity.getBody();
