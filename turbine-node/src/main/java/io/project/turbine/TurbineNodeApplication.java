2b7b58ba32c2cfcda4730331c5d01cb20d74aa70
ARMEN ARZUMANYAN <armen.arzumanyan.external@atos.net>
2017-10-10 17:05:48 +0400

    up to date

--- turbine-node/src/main/java/io/project/turbine/TurbineNodeApplication.java
+++ turbine-node/src/main/java/io/project/turbine/TurbineNodeApplication.java
@@ -3,9 +3,11 @@ package io.project.turbine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableTurbine
 public class TurbineNodeApplication {
 
 	public static void main(String[] args) {
