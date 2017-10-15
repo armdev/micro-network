1397ec3b231e9705f9990485fdf1cf3e7ad9f71d
armdev <armen.arzumanyan@gmail.com>
2017-10-15 22:53:35 +0400

    added remote server

--- frontend-node/src/main/java/com/project/web/handlers/UserBean.java
+++ frontend-node/src/main/java/com/project/web/handlers/UserBean.java
@@ -6,26 +6,30 @@ import lombok.Data;
 
 /**
  *
- * @author armdev   
+ * @author armdev
  */
 @Named(value = "userBean")
 @RequestScoped
 @Data
 public class UserBean {
-    
+
     private String email;
-  
+
     private String password;
 
     public UserBean() {
+        System.out.println("Generate date " + System.currentTimeMillis());
+        System.out.println("Generate date " + System.currentTimeMillis());
+        System.out.println("Generate date " + System.currentTimeMillis());
+        System.out.println("Generate date " + System.currentTimeMillis());
+        System.out.println("Generate date " + System.currentTimeMillis());
     }
-    
-    public String doLogin(){
+
+    public String doLogin() {
         System.out.println("Email is " + email);
         System.out.println("Password  is " + password);
-        
+
         return "profile";
     }
 
- 
 }
