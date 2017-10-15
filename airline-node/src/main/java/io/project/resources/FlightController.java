dca9355588fa3ca9bf6a345c554f4c6f075ef290
armdev <armen.arzumanyan@gmail.com>
2017-10-15 20:30:33 +0400

    added remote server

--- airline-node/src/main/java/io/project/resources/FlightController.java
+++ airline-node/src/main/java/io/project/resources/FlightController.java
@@ -1,67 +1,41 @@
 package io.project.resources;
 
-
 import io.project.model.Flight;
 import io.project.services.FlightService;
 import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
-import static org.springframework.web.bind.annotation.RequestMethod.PUT;
-
-/**
- *
- * @author Admin
- */
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api")
 public class FlightController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(FlightController.class);
+    private static final Logger log = LoggerFactory.getLogger(FlightController.class);
 
     @Autowired
     private FlightService flightService;
 
-    @RequestMapping(method = GET)
-    public List<Flight> list() {
-        LOGGER.info("Fetching all list");
-        return flightService.listAll();
+    @GetMapping(path="/flight/{id}" , produces = "application/json;charset=UTF-8")
+    public ResponseEntity<Flight> getFlight(@PathVariable Long id) {
+        log.debug("REST request to get Blog : {}", id);
+        Flight flight = flightService.getById(id);
+        return ResponseUtil.wrapOrNotFound(Optional.ofNullable(flight));
     }
 
-    @RequestMapping(value = "/{id}", method = GET)
-    public Flight get(@PathVariable Long id) {
-        LOGGER.info("Find by id ");
-        return flightService.getById(id);
+    @GetMapping("/flights")
+    public List<Flight> findAll() {
+        log.debug("REST request to get all Blogs");
+        List<Flight> flightList = flightService.listAll();
+        return flightList;
     }
-
-    @RequestMapping(value = "/{id}", method = PUT)
-    public ResponseEntity<Flight> put(@PathVariable Long id, @RequestBody Flight flight) {
-        LOGGER.info("Update by id ");
-        flight.setId(id);
-       
-        return ResponseEntity.accepted().body(flightService.saveOrUpdate(flight));
-    }
-
-    @RequestMapping(value = "/{id}", method = POST)
-    public ResponseEntity<Flight> post(@RequestBody Flight flight) {
-        LOGGER.info("Save new object");      
-        return ResponseEntity.accepted().body(flightService.saveOrUpdate(flight));
-    }
-
-    @RequestMapping(value = "/{id}", method = DELETE)
-    public ResponseEntity<Object> delete(@PathVariable String id) {
-        LOGGER.info("Delete object");
-        return null;
-    }
-
+//https://docs.spring.io/spring/docs/current/spring-framework-reference/web.html#mvc
 }
